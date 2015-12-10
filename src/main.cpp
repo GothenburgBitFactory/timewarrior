c693e9d3101ef4b289fd07b989191101992ac0f5
Paul Beckingham <paul@beckingham.net>
2015-12-10 09:31:48 -0500

    Framework: Added program framework and UTF8 code

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2015, Paul Beckingham, Federico Hernandez.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// http://www.opensource.org/licenses/mit-license.php
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cmake.h>
+#include <iostream>
+#include <new>
+#include <cstring>
+
+////////////////////////////////////////////////////////////////////////////////
+int main (int argc, const char** argv)
+{
+  int status = 0;
+
+  // Lightweight version checking that doesn't require initialization or any I/O.
+  if (argc == 2 && ! strcmp (argv[1], "--version"))
+  {
+    std::cout << VERSION << "\n";
+  }
+  else
+  {
+    try
+    {
+      // TODO Initialize configuration.
+      // TODO Dispatch to commands.
+    }
+
+    catch (const std::string& error)
+    {
+      std::cerr << error << "\n";
+      status = -1;
+    }
+
+    catch (std::bad_alloc& error)
+    {
+      std::cerr << "Error: Memory allocation failed: " << error.what () << "\n";
+      status = -3;
+    }
+
+    catch (...)
+    {
+      std::cerr << "Error: Unknown problem, please report.\n";
+      status = -2;
+    }
+  }
+
+  return status;
+}
+
+////////////////////////////////////////////////////////////////////////////////
