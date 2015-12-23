ed138fc8196f8b9c03ac3c025429bf9fafcb3f24
Paul Beckingham <paul@beckingham.net>
2015-12-22 23:43:57 -0500

    gr: Removed unused code

--- src/gr.cpp
+++ src/gr.cpp
@@ -50,9 +50,6 @@ int main (int argc, char** argv)
   // Process command line arguments
   std::string grammarFile = "";
   std::string commandLine = "";
-/*
-  std::vector <std::string> args;
-*/
 
   for (int i = 1; i < argc; ++i)
   {
