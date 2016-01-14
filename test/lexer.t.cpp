3164d08e64db3afae2e1db97cd6e91cf45a90e84
Paul Beckingham <paul@beckingham.net>
2016-01-14 17:29:04 -0500

    Tests: Removed Lexer::isWhitespace tests

--- test/lexer.t.cpp
+++ test/lexer.t.cpp
@@ -34,40 +34,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 int main (int, char**)
 {
-  UnitTest t (371);
+  UnitTest t (344);
 
   std::vector <std::pair <std::string, Lexer::Type>> tokens;
   std::string token;
   Lexer::Type type;
-
-  // White space detection.
-  t.notok (Lexer::isWhitespace (0x0041), "U+0041 (A) ! isWhitespace");
-  t.ok (Lexer::isWhitespace (0x0020), "U+0020 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x0009), "U+0009 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x000A), "U+000A isWhitespace");
-  t.ok (Lexer::isWhitespace (0x000B), "U+000B isWhitespace");
-  t.ok (Lexer::isWhitespace (0x000C), "U+000C isWhitespace");
-  t.ok (Lexer::isWhitespace (0x000D), "U+000D isWhitespace");
-  t.ok (Lexer::isWhitespace (0x0085), "U+0085 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x00A0), "U+00A0 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x1680), "U+1680 isWhitespace"); // 10
-  t.ok (Lexer::isWhitespace (0x180E), "U+180E isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2000), "U+2000 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2001), "U+2001 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2002), "U+2002 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2003), "U+2003 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2004), "U+2004 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2005), "U+2005 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2006), "U+2006 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2007), "U+2007 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2008), "U+2008 isWhitespace"); // 20
-  t.ok (Lexer::isWhitespace (0x2009), "U+2009 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x200A), "U+200A isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2028), "U+2028 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x2029), "U+2029 isWhitespace");
-  t.ok (Lexer::isWhitespace (0x202F), "U+202F isWhitespace");
-  t.ok (Lexer::isWhitespace (0x205F), "U+205F isWhitespace");
-  t.ok (Lexer::isWhitespace (0x3000), "U+3000 isWhitespace");
 
   // static bool Lexer::dequote (std::string&, const std::string& quotes = "'\"");
   t.is (Lexer::dequote ("foo"),         "foo",       "Lexer::dequote foo --> foo");
