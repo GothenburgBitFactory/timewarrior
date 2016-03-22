3e16f52621708c8718127e6ccb51e672ff1a0c35
Paul Beckingham <paul@beckingham.net>
2016-03-21 23:46:58 -0400

    Datafile: Implemented ::getIntervalsSince

--- src/Datafile.cpp
+++ src/Datafile.cpp
@@ -65,9 +65,20 @@ Interval Datafile::getLatestInterval ()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector <Interval> Datafile::getAllIntervalsSince (Datetime)
+std::vector <Interval> Datafile::getAllIntervalsSince (Datetime when)
 {
-  // TODO Load data
+  if (when >= _day1 && when <= _dayN)
+  {
+    if (! _intervals_loaded)
+      load_intervals ();
+
+    // Rely on intervals being sorted by start time.
+    std::vector <Interval>::iterator i;
+    for (i = _intervals.begin (); i != _intervals.end (); i++)
+      if (i->start () >= when)
+        return std::vector <Interval> (i, _intervals.end ());
+  }
+
   return {};
 }
 
