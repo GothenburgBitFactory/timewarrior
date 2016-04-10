0b9f25bfe07ead2164641f56a957f4c4be43b64f
Paul Beckingham <paul@beckingham.net>
2016-04-10 10:34:52 -0400

    lex: Added test harness for lexer

---
+++ src/lex.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <Lexer.h>
+
+int main (int argc, char** argv)
+{
+  for (auto i = 1; i < argc; i++)
+  {
+    std::cout << "argument '" << argv[i] << "'\n";
+
+    Lexer l (argv[i]);
+    std::string token;
+    Lexer::Type type;
+    while (l.token (token, type))
+      std::cout << "  token '" << token << "' " << Lexer::typeToString (type) << "\n";
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
