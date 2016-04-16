bb4fded91996c871fa9cf2e382ca60c7abde52b2
Paul Beckingham <paul@beckingham.net>
2016-04-16 09:13:56 -0400

    Tests: Added ::overlap tests

--- test/daterange.t.cpp
+++ test/daterange.t.cpp
@@ -31,7 +31,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 int main (int, char**)
 {
-  UnitTest t (6);
+  UnitTest t (21);
 
   // bool isStarted () const;
   // bool isEnded () const;
@@ -48,6 +48,57 @@ int main (int, char**)
   i1.end (Datetime ());
   t.is (i1.isStarted (), true, "Daterange(start=now,end=now).isStarted -> true");
   t.is (i1.isEnded (),   true, "Daterange(start=now,end=now).isEnded -> true");
+
+  // this                     |--------|
+  // other      |--------|                                   [A] false
+  // other               |--------|                          [B] true
+  // other                      |----|                       [C] true
+  // other                         |--------|                [D] true
+  // other                                  |--------|       [E] false
+  // other                  |-------------|                  [F] true
+  // other                  |...                             [G] true
+  // other                       |...                        [H] true
+  // other                               |...                [I] false
+  Daterange refClosed;
+  refClosed.start (Datetime (6,  1, 2016));
+  refClosed.end   (Datetime (6, 30, 2016));
+
+  Daterange testA; testA.start (Datetime (4,  1, 2016)); testA.end (Datetime (4, 30, 2016));
+  Daterange testB; testB.start (Datetime (5, 15, 2016)); testB.end (Datetime (6, 15, 2016));
+  Daterange testC; testC.start (Datetime (6, 10, 2016)); testC.end (Datetime (6, 20, 2016));
+  Daterange testD; testD.start (Datetime (6, 15, 2016)); testD.end (Datetime (7, 15, 2016));
+  Daterange testE; testE.start (Datetime (8,  1, 2016)); testE.end (Datetime (8, 31, 2016));
+  Daterange testF; testF.start (Datetime (5, 15, 2016)); testF.end (Datetime (7, 15, 2016));
+  Daterange testG; testG.start (Datetime (5, 15, 2016));
+  Daterange testH; testH.start (Datetime (6, 15, 2016));
+  Daterange testI; testI.start (Datetime (7, 15, 2016));
+
+  t.notok (refClosed.overlap (testA), "Daterange: ! refClosed.overlap(testA)");
+  t.ok    (refClosed.overlap (testB), "Daterange:   refClosed.overlap(testB)");
+  t.ok    (refClosed.overlap (testC), "Daterange:   refClosed.overlap(testC)");
+  t.ok    (refClosed.overlap (testD), "Daterange:   refClosed.overlap(testD)");
+  t.notok (refClosed.overlap (testE), "Daterange: ! refClosed.overlap(testE)");
+  t.ok    (refClosed.overlap (testF), "Daterange:   refClosed.overlap(testF)");
+  t.ok    (refClosed.overlap (testG), "Daterange:   refClosed.overlap(testG)");
+  t.ok    (refClosed.overlap (testH), "Daterange:   refClosed.overlap(testH)");
+  t.notok (refClosed.overlap (testI), "Daterange: ! refClosed.overlap(testI)");
+
+  // this                     |...
+  // other      |--------|                                   [A] false
+  // other               |--------|                          [B] true
+  // other                      |----|                       [C] true
+  // other                  |...                             [G] true
+  // other                       |...                        [H] true
+  // other                               |...                [I] true
+  Daterange refOpen;
+  refOpen.start (Datetime (6, 1, 2016));
+
+  t.notok (refOpen.overlap (testA), "Daterange: ! refOpen.overlap(testA)");
+  t.ok    (refOpen.overlap (testB), "Daterange:   refOpen.overlap(testB)");
+  t.ok    (refOpen.overlap (testC), "Daterange:   refOpen.overlap(testC)");
+  t.ok    (refOpen.overlap (testG), "Daterange:   refOpen.overlap(testG)");
+  t.ok    (refOpen.overlap (testH), "Daterange:   refOpen.overlap(testH)");
+  t.ok    (refOpen.overlap (testI), "Daterange: ! refOpen.overlap(testI)");
 
   return 0;
 }
