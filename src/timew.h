0ee0aaa8019777995fa32cd584b8d7847c579a03
Paul Beckingham <paul@beckingham.net>
2016-04-16 15:29:01 -0400

    helper: Removed obsolete intervalToFileNames

--- src/timew.h
+++ src/timew.h
@@ -54,7 +54,6 @@ Timeline createTimelineFromData (const Rules&, Database&, const Filter&);
 Interval getLatestInterval (Database&);
 bool intervalMatchesFilter (const Interval&, const Filter&);
 std::string jsonFromIntervals (const std::vector <Interval>&);
-std::vector <std::string> intervalToFileNames (const Interval&);
 
 // utiŀ.cpp
 std::string osName ();
