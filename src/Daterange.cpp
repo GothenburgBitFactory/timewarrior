bd4b4069ea41f8f4729a0a542aab424a889d58f6
Paul Beckingham <paul@beckingham.net>
2016-04-16 08:40:25 -0400

    Daterange: Added object to handle overlapping date ranges

---
+++ src/Daterange.cpp
@@ -0,0 +1,113 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2015 - 2016, Paul Beckingham, Federico Hernandez.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// http://www.opensource.org/licenses/mit-license.php
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cmake.h>
+#include <Daterange.h>
+
+////////////////////////////////////////////////////////////////////////////////
+Daterange::Daterange (const Datetime start, const Datetime end)
+{
+  _start = start;
+  _end = end;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Datetime Daterange::start () const
+{
+  return _start;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Daterange::start (Datetime value)
+{
+  _start = value;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Datetime Daterange::end () const
+{
+  return _end;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Daterange::end (Datetime value)
+{
+  _end = value;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+bool Daterange::isStarted () const
+{
+  return _start.toEpoch () > 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+bool Daterange::isEnded () const
+{
+  return _end.toEpoch   () > 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Detect the following cases:
+//
+// this                     |--------|
+// other      |--------|                                   false  [1]
+// other               |--------|                          true
+// other                      |----|                       true
+// other                         |--------|                true
+// other                                  |--------|       false  [2]
+// other                  |-------------|                  true
+// other                  |...                             true
+// other                       |...                        true
+// other                               |...                false  [3]
+//
+// this                     |...
+// other      |--------|                                   false  [4]
+// other               |--------|                          true
+// other                      |----|                       true
+// other                  |...                             true
+// other                      |...                         true
+//
+bool Daterange::overlap (const Daterange other) const
+{
+  if (! isStarted () ||
+      ! other.isStarted ())
+    return false;
+
+  // [1], [4] Other range ends before this range starts.
+  if (other.isEnded () &&
+      other.end () <= start ())
+    return false;
+
+  // [2], [3] Other range starts after this range ends.
+  if (isEnded () &&
+      other.start () >= end ())
+    return false;
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/Daterange.h
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2015 - 2016, Paul Beckingham, Federico Hernandez.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// http://www.opensource.org/licenses/mit-license.php
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef INCLUDED_DATERANGE
+#define INCLUDED_DATERANGE
+
+#include <Datetime.h>
+
+class Daterange
+{
+public:
+  Daterange () = default;
+  Daterange (const Datetime, const Datetime);
+
+  Datetime start () const;
+  void start (Datetime);
+
+  Datetime end () const;
+  void end (Datetime);
+
+  bool isStarted () const;
+  bool isEnded () const;
+  bool overlap (const Daterange) const;
+
+private:
+  Datetime               _start {0};
+  Datetime               _end   {0};
+};
+
+#endif
