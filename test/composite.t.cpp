fe418581896345add44a2492d371c33b86e7dbc6
Paul Beckingham <paul@beckingham.net>
2016-04-24 22:37:31 -0400

    Tests: Added extreme composite test

--- test/composite.t.cpp
+++ test/composite.t.cpp
@@ -31,7 +31,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 int main (int, char**)
 {
-  UnitTest t (2);
+  UnitTest t (3);
 
   Composite c1;
   c1.add ("left",  2, Color ());
@@ -54,6 +54,64 @@ int main (int, char**)
   c4.add ("bbbbb",       5, Color ("white on blue"));
   c4.add ("c",          15, Color ("white on green"));
   t.diag (c4.str ());
+
+  Composite c5;
+  c5.add ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0, Color ());
+  c5.add ("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",   1, Color ());
+  c5.add ("ccccccccccccccccccccccccccccccccccccccccccccccc",     2, Color ());
+  c5.add ("ddddddddddddddddddddddddddddddddddddddddddddd",       3, Color ());
+  c5.add ("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",         4, Color ());
+  c5.add ("fffffffffffffffffffffffffffffffffffffffff",           5, Color ());
+  c5.add ("ggggggggggggggggggggggggggggggggggggggg",             6, Color ());
+  c5.add ("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",               7, Color ());
+  c5.add ("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",                 8, Color ());
+  c5.add ("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj",                   9, Color ());
+  c5.add ("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",                    10, Color ());
+  c5.add ("lllllllllllllllllllllllllllll",                      11, Color ());
+  c5.add ("mmmmmmmmmmmmmmmmmmmmmmmmmmm",                        12, Color ());
+  c5.add ("nnnnnnnnnnnnnnnnnnnnnnnnn",                          13, Color ());
+  c5.add ("ooooooooooooooooooooooo",                            14, Color ());
+  c5.add ("ppppppppppppppppppppp",                              15, Color ());
+  c5.add ("qqqqqqqqqqqqqqqqqqq",                                16, Color ());
+  c5.add ("rrrrrrrrrrrrrrrrr",                                  17, Color ());
+  c5.add ("sssssssssssssss",                                    18, Color ());
+  c5.add ("ttttttttttttt",                                      19, Color ());
+  c5.add ("uuuuuuuuuuu",                                        20, Color ());
+  c5.add ("vvvvvvvvv",                                          21, Color ());
+  c5.add ("wwwwwww",                                            22, Color ());
+  c5.add ("xxxxx",                                              23, Color ());
+  c5.add ("yyy",                                                24, Color ());
+  c5.add ("z",                                                  25, Color ());
+  t.is (c5.str (), "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba", "Composite ... --> 'abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba'");
+
+  Composite c6;
+  c6.add ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0, Color ("blue on gray0"));
+  c6.add ("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",   1, Color ("blue on gray1"));
+  c6.add ("ccccccccccccccccccccccccccccccccccccccccccccccc",     2, Color ("blue on gray2"));
+  c6.add ("ddddddddddddddddddddddddddddddddddddddddddddd",       3, Color ("blue on gray3"));
+  c6.add ("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",         4, Color ("blue on gray4"));
+  c6.add ("fffffffffffffffffffffffffffffffffffffffff",           5, Color ("blue on gray5"));
+  c6.add ("ggggggggggggggggggggggggggggggggggggggg",             6, Color ("blue on gray6"));
+  c6.add ("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",               7, Color ("blue on gray7"));
+  c6.add ("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",                 8, Color ("blue on gray8"));
+  c6.add ("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj",                   9, Color ("blue on gray9"));
+  c6.add ("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",                    10, Color ("blue on gray10"));
+  c6.add ("lllllllllllllllllllllllllllll",                      11, Color ("blue on gray11"));
+  c6.add ("mmmmmmmmmmmmmmmmmmmmmmmmmmm",                        12, Color ("blue on gray12"));
+  c6.add ("nnnnnnnnnnnnnnnnnnnnnnnnn",                          13, Color ("blue on gray13"));
+  c6.add ("ooooooooooooooooooooooo",                            14, Color ("blue on gray14"));
+  c6.add ("ppppppppppppppppppppp",                              15, Color ("blue on gray15"));
+  c6.add ("qqqqqqqqqqqqqqqqqqq",                                16, Color ("blue on gray16"));
+  c6.add ("rrrrrrrrrrrrrrrrr",                                  17, Color ("blue on gray17"));
+  c6.add ("sssssssssssssss",                                    18, Color ("blue on gray18"));
+  c6.add ("ttttttttttttt",                                      19, Color ("blue on gray19"));
+  c6.add ("uuuuuuuuuuu",                                        20, Color ("blue on gray20"));
+  c6.add ("vvvvvvvvv",                                          21, Color ("blue on gray21"));
+  c6.add ("wwwwwww",                                            22, Color ("blue on gray22"));
+  c6.add ("xxxxx",                                              23, Color ("blue on gray23"));
+  c6.add ("yyy",                                                24, Color ("blue on bright white"));
+  c6.add ("z",                                                  25, Color ("blue on bright white"));
+  t.diag (c6.str ());
 
   return 0;
 }
