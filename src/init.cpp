3bc1acdafddc41d202019d27062fde2d8da0207d
Paul Beckingham <paul@beckingham.net>
2016-05-06 00:16:14 -0400

    init: Dispatch to CmdShow

--- src/init.cpp
+++ src/init.cpp
@@ -61,7 +61,6 @@ void initializeEntities (CLI& cli)
   cli.entity ("command", "clear");
   cli.entity ("command", "config");
   cli.entity ("command", "continue");
-  cli.entity ("command", "define");
   cli.entity ("command", "diagnostics");
   cli.entity ("command", "export");
   cli.entity ("command", "extensions");
@@ -69,6 +68,7 @@ void initializeEntities (CLI& cli)
   cli.entity ("command", "help");
   cli.entity ("command", "import");
   cli.entity ("command", "report");
+  cli.entity ("command", "show");
   cli.entity ("command", "start");
   cli.entity ("command", "stop");
   cli.entity ("command", "tags");
@@ -230,22 +230,23 @@ int dispatchCommand (
     else if (command == "clear")       status = CmdClear         (                                );
     else if (command == "config")      status = CmdConfig        (cli, rules                      );
     else if (command == "continue")    status = CmdContinue      (     rules, database            );
+    else if (command == "day")         status = CmdReportDay     (cli, rules, database            );
     else if (command == "diagnostics") status = CmdDiagnostics   (     rules, database, extensions);
     else if (command == "export")      status = CmdExport        (cli, rules, database            );
     else if (command == "extensions")  status = CmdExtensions    (     rules,           extensions);
     else if (command == "gaps")        status = CmdGaps          (cli, rules, database            );
     else if (command == "help")        status = CmdHelp          (cli                             );
     else if (command == "import")      status = CmdImport        (                                );
-    else if (command == "report")      status = CmdReport        (cli, rules, database, extensions);
-    else if (command == "day")         status = CmdReportDay     (cli, rules, database            );
-    else if (command == "week")        status = CmdReportWeek    (cli, rules, database            );
     else if (command == "month")       status = CmdReportMonth   (cli, rules, database            );
+    else if (command == "report")      status = CmdReport        (cli, rules, database, extensions);
+    else if (command == "show")        status = CmdShow          (     rules                      );
     else if (command == "start")       status = CmdStart         (cli, rules, database            );
     else if (command == "stop")        status = CmdStop          (cli, rules, database            );
     else if (command == "summary")     status = CmdReportSummary (cli, rules, database            );
     else if (command == "tags")        status = CmdTags          (     rules, database            );
     else if (command == "track")       status = CmdTrack         (cli, rules, database            );
     else if (command == "undo")        status = CmdUndo          (                                );
+    else if (command == "week")        status = CmdReportWeek    (cli, rules, database            );
     else                               status = CmdReport        (cli, rules, database, extensions);
   }
   else if (cli._args.size () == 1)
