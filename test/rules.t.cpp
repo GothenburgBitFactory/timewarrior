db1b1410bdca82267b5e4c61fef20f2156f2c10d
Paul Beckingham <paul@beckingham.net>
2016-05-20 15:28:51 -0500

    Tests: Corrected number of rules defaults

--- test/rules.t.cpp
+++ test/rules.t.cpp
@@ -45,7 +45,7 @@ int main (int, char**)
   r.set ("one.two",       12);
   r.set ("one.two.three", 123);
   r.set ("one.two.four",  124);
-  t.is ((int) r.all ().size (), 14,      "Rules all (\"\") --> 14");
+  t.is ((int) r.all ().size (), 23,      "Rules all (\"\") --> 23");
   t.ok (r.all ("one.two").size () == 3,  "Rules all (\"one.two\") --> 3");
 
   return 0;
