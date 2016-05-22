a257ccacc8ceb7d9a2467e5b1f7a2d71799ecdd8
Paul Beckingham <paul@beckingham.net>
2016-05-21 20:55:40 -0500

    Ext: Added csv.py example extension

---
+++ ext/csv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+# Skip the configuration settings.
+for line in sys.stdin:
+  if line == '\n':
+    break;
+
+# Extract the JSON.
+doc = ''
+for line in sys.stdin:
+  doc += line
+
+total_active_time = 0
+
+j = json.loads(doc)
+for object in j:
+  line = '"%s",' % object['start']
+  line +='"%s"' % (object['end'] if 'end' in object else '')
+
+  if 'tags' in object:
+    for tag in object['tags']:
+      line += ',"%s"' % tag
+
+  print line
