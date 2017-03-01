310f48973e03df89eb6a4fb7b4d1bcab1f83572c
Thomas Lauf <thomas.lauf@tngtech.com>
2017-03-01 20:42:32 +0100

    extensions: Make extensions conform with PEP8

--- ext/csv.py
+++ ext/csv.py
@@ -5,23 +5,23 @@ import json
 
 # Skip the configuration settings.
 for line in sys.stdin:
-  if line == '\n':
-    break;
+    if line == '\n':
+        break;
 
 # Extract the JSON.
 doc = ''
 for line in sys.stdin:
-  doc += line
+    doc += line
 
 total_active_time = 0
 
 j = json.loads(doc)
 for object in j:
-  line = '"%s",' % object['start']
-  line +='"%s"' % (object['end'] if 'end' in object else '')
+    line = '"%s",' % object['start']
+    line +='"%s"' % (object['end'] if 'end' in object else '')
 
-  if 'tags' in object:
-    for tag in object['tags']:
-      line += ',"%s"' % tag
+    if 'tags' in object:
+        for tag in object['tags']:
+            line += ',"%s"' % tag
 
-  print line
+    print line
