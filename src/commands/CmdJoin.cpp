GothenburgBitFactory/timewarrior

commit 8ce9e83ab53feeeef683593853f5e6c6e9e44677

TI-93 #97 Make CLI::getIds() return a set to guarantee uniqueness of ids

src/commands/CmdJoin.cpp
@@ -39,7 +39,7 @@ int CmdJoin (
   Database& database)
 {
   // Gather IDs and TAGs.
-  std::vector <int> ids = cli.getIds();
+  std::set <int> ids = cli.getIds ();
 
   // Only 2 IDs allowed in a join.
   if (ids.size () != 2)
@@ -52,20 +52,25 @@ int CmdJoin (
 
   // ID values must be in range.
   for (auto& id : ids)
+  {
     if (id > static_cast <int> (tracked.size ()))
+    {
       throw format ("ID '@{1}' does not correspond to any tracking.", id);
+    }
 
-  auto first_id  = std::max (ids[0], ids[1]);
-  auto second_id = std::min (ids[0], ids[1]);
+  }
+
+  auto first_id  = std::min (*ids.begin (), *ids.end ());
+  auto second_id = std::max (*ids.begin (), *ids.end ());
+
   Interval first  = tracked[tracked.size () - first_id];
   Interval second = tracked[tracked.size () - second_id];
 
   // TODO Require confirmation if intervals are not consecutive.
   // TODO Require confirmation if tags don't match.
 
-  auto combined = first;
-  combined.range.end = second.range.end;
-
+  auto combined = second;
+  combined.range.end = first.range.end;
   database.deleteInterval (first);
   database.deleteInterval (second);
 
@@ -73,7 +78,7 @@ int CmdJoin (
   database.addInterval (combined);
 
   if (rules.getBoolean ("verbose"))
-    std::cout << "Joined @" << ids[0] << " and @" << ids[1] << '\n';
+    std::cout << "Joined @" << first_id << " and @" << second_id << '\n';
 
   return 0;
 }

src/CLI.cpp
@@ -33,6 +33,7 @@
 #include <utf8.h>
 #include <sstream>
 #include <algorithm>
+#include <set>
 
 ////////////////////////////////////////////////////////////////////////////////
 A2::A2 (const std::string& raw, Lexer::Type lextype)
@@ -524,14 +525,14 @@ bool CLI::exactMatch (
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector<int> CLI::getIds() const
+std::set<int> CLI::getIds() const
 {
-  std::vector <int> ids;
+  std::set <int> ids;
 
   for (auto& arg : _args)
   {
     if (arg.hasTag ("ID"))
-      ids.push_back (strtol (arg.attribute ("value").c_str (), NULL, 10));
+      ids.insert (strtol (arg.attribute ("value").c_str (), NULL, 10));
   }
 
   return ids;

src/CLI.h
@@ -29,6 +29,7 @@
 
 #include <Lexer.h>
 #include <string>
+#include <set>
 #include <vector>
 #include <map>
 
@@ -64,7 +65,7 @@ public:
   bool canonicalize (std::string&, const std::string&, const std::string&) const;
   std::string getBinary () const;
   std::string getCommand () const;
-  std::vector <int> getIds () const;
+  std::set <int> getIds () const;
   std::string dump (const std::string& title = "CLI Parser") const;
 
 private:

src/commands/CmdContinue.cpp
@@ -36,7 +36,7 @@ int CmdContinue (
   Database& database)
 {
   // Gather IDs and TAGs.
-  std::vector <int> ids = cli.getIds();
+  std::set <int> ids = cli.getIds();
 
   if (ids.size() > 1)
     throw std::string ("You can only specify one ID to continue.");
@@ -51,10 +51,12 @@ int CmdContinue (
     Interval filter;
     auto tracked = getTracked (database, rules, filter);
 
-    if (ids[0] > static_cast <int> (tracked.size ()))
-      throw format ("ID '@{1}' does not correspond to any tracking.", ids[0]);
+    auto id = *ids.begin ();
 
-    to_copy = tracked[tracked.size () - ids[0]];
+    if (id > static_cast <int> (tracked.size ()))
+      throw format ("ID '@{1}' does not correspond to any tracking.", id);
+
+    to_copy = tracked[tracked.size () - id];
   }
   else
   {

src/commands/CmdDelete.cpp
@@ -35,7 +35,7 @@ int CmdDelete (
   Database& database)
 {
   // Gather IDs.
-  std::vector <int> ids = cli.getIds();
+  std::set <int> ids = cli.getIds ();
 
   if (ids.empty ())
     throw std::string ("IDs must be specified. See 'timew help delete'.");

src/commands/CmdFill.cpp
@@ -36,7 +36,7 @@ int CmdFill (
   Rules& rules,
   Database& database)
 {
-  std::vector <int> ids = cli.getIds();
+  std::set <int> ids = cli.getIds ();
 
   if (ids.empty ())
     throw std::string ("IDs must be specified. See 'timew help fill'.");

src/commands/CmdLengthen.cpp
@@ -39,7 +39,7 @@ int CmdLengthen (
   Database& database)
 {
   // Gather IDs and TAGs.
-  std::vector <int> ids = cli.getIds();
+  std::set <int> ids = cli.getIds ();
 
   if (ids.empty ())
     throw std::string ("IDs must be specified. See 'timew help lengthen'.");

src/commands/CmdMove.cpp
@@ -39,7 +39,7 @@ int CmdMove (
   Database& database)
 {
   // Gather ID and TAGs.
-  std::vector<int> ids = cli.getIds();
+  std::set <int> ids = cli.getIds ();
 
   if (ids.size() > 1)
   {
@@ -51,7 +51,7 @@ int CmdMove (
     throw std::string ("ID must be specified. See 'timew help move'.");
   }
 
-  int id = ids[0];
+  int id = *ids.begin ();
 
   std::string new_start;
   for (auto& arg : cli._args)

commit 2f5a3f2f130728dd71faf1f55efd61fb176d66b4

CmdJoin: Fix segment fault on join.
Code was attempting to dereference the end () iterator, which is invalid.

Since sets are already ordered, we can use the first and last element directly
instead of using the min/max functions.

Fixes a bug introduced in ( 8ce9e83ab53 "TI-93 #97 Make CLI::getIds() return a
set to guarantee uniqueness of ids" )

src/commands/CmdJoin.cpp
@@ -62,8 +62,8 @@ int CmdJoin (
 
   database.startTransaction ();
 
-  auto first_id  = std::min (*ids.begin (), *ids.end ());
-  auto second_id = std::max (*ids.begin (), *ids.end ());
+  auto first_id  = *ids.begin ();
+  auto second_id = *ids.rbegin ();
 
   Interval first  = tracked[tracked.size () - first_id];
   Interval second = tracked[tracked.size () - second_id];