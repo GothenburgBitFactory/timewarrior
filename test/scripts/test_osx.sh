eccf82ff5631cd28b93b77212255584b63e78c6f
Thomas Lauf <thomas.lauf@tngtech.com>
2019-12-21 22:55:02 +0100

    Print version information at end
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- test/scripts/test_osx.sh
+++ test/scripts/test_osx.sh
@@ -9,10 +9,14 @@ git submodule update
 cmake -DCMAKE_BUILD_TYPE=debug .
 make -j2
 make install
-timew --version
 
 pushd test || exit
 make
 ./run_all -v
 grep 'not ok' all.log
 ./problems
+echo "timew $( timew --version )"
+python --version
+python3 --version
+cmake --version
+clang --version
