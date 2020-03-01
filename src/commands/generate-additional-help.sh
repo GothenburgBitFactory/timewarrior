888e71578204effd70b54e9346176663a18c12df
Thomas Lauf <thomas.lauf@tngtech.com>
2020-03-01 19:01:06 +0100

    Streamline generation of additional help
    - do filtering outside CmdHelp.cpp
    - generate only concepts
    - use join from libshared
    
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- src/commands/generate-additional-help.sh
+++ src/commands/generate-additional-help.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-echo "// Generated file. Do not modify"
-echo "static const char * documented_concepts[] = {"
-for command in $(/bin/ls doc/man7/*.in | /bin/sed -n 's|doc/man7/timew-\(\w\+\).7.in|\1|p' | /usr/bin/sort); do
-  echo "  \"${command}\","
-done
-echo "};"
+CONCEPTS="$( ls doc/man7/*.in | sed -E 's|doc/man7/timew-(.*).7.in|\1|' | sort )"
+COMMANDS="$( ls doc/man1/*.in | sed -E 's|doc/man1/timew-(.*).1.in|\1|' | sort | tr '\n' ' ' )"
 
-echo "static const char * documented_commands[] = {"
-for command in $(/bin/ls doc/man1/*.in | /bin/sed -n 's|doc/man1/timew-\(\w\+\).1.in|\1|p' | /usr/bin/sort); do
-  echo "  \"${command}\","
+echo "// Generated file. Do not modify"
+echo "#include <vector>"
+echo "#include <string>"
+echo "static const std::vector <std::string> timew_help_concepts = {"
+for concept in ${CONCEPTS} ; do
+   ( echo "${COMMANDS}" | grep -vq "${concept}" ) && echo "  \"${concept}\","
 done
 echo "};"
