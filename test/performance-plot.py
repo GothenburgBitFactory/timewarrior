8d1b89db30ba4d523c938ec951dc7bf20d3b1d37
Thomas Lauf <thomas.lauf@tngtech.com>
2021-11-19 22:26:16 +0100

    Improve performance-plot script
    - Add option to select commands for display
    - Use name of data directory as plot title
    
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- test/performance-plot.py
+++ test/performance-plot.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
-import argparse
+
 import os
 
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
 parser = argparse.ArgumentParser(description='Display performance plots.')
-parser.add_argument('output_dir',
-                    help='directory containing the measurement files')
+parser.add_argument('output_dir', help='directory containing the measurement files')
+parser.add_argument('commands', nargs='*', type=str)
 
 args = parser.parse_args()
 
 output_directory = args.output_dir
+commands = args.commands
 
 plt.axes([0.1, 0.1, 0.6, 0.75])
 plt.xlabel("# database entries")
 plt.ylabel("time [s]")
-plt.title("timew performance")
+plt.title(output_directory)
 
 for filename in os.listdir(output_directory):
     if filename.endswith(".log"):
         cmd = "-".join(filename.split('-')[1:-1])
+    else:
+        continue
+
+    if len(commands) == 0 or cmd in commands:
         try:
             x, y = np.loadtxt(os.path.join(output_directory, filename),
                               delimiter='\t',
                               usecols=(1, 2),
                               unpack=True)
-            plt.plot(x, y, label=cmd, marker=".", linestyle='')
+            plt.plot(x, y, label=cmd, marker=".", linestyle='-')
         except ValueError as e:
             print("Invalid file: {} {}".format(filename, e))
 
