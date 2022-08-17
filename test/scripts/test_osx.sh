a6322b4d71ec89169dbbe618dcdc1dc4ad77d629
Thomas Lauf <thomas.lauf@tngtech.com>
2022-08-17 20:44:45 +0200

    Use python 10 on macOS runners
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- test/scripts/test_osx.sh
+++ test/scripts/test_osx.sh
@@ -7,8 +7,8 @@ brew install asciidoctor
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$( pyenv init --path )"
-pyenv install 3.8.0
-pyenv global 3.8.0
+pyenv install 3.10.0
+pyenv global 3.10.0
 pip install --upgrade pip
 pip install python-dateutil
 
