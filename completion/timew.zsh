27f9df30d95108c5c2c2cd6d740f00b2c59e33d1
Thomas Lauf <thomas.lauf@tngtech.com>
2026-08-02 18:17:45 +0200

    Update ZSH completion to bdd8f7b4e25faee417a154c1480f615a69b99406

--- completion/timew.zsh
+++ completion/timew.zsh
@@ -60,11 +60,13 @@ _timew() {
 
 _hints=(
     "\:adjust:Automatically correct overlaps"
+    "\:all:All tracked time"
     "\:blank:Leaves tracked time out of a report"
     "\:color:Force color on, even if not connected to a TTY"
     "\:day:The 24 hours of the current day"
     "\:debug:Runs in debug mode, shows many runtime details"
     "\:fill:Expand time to fill surrounding available gap"
+    "\:fortnight:This week and the one before"
     "\:ids:Displays interval ID numbers in the summary report"
     "\:lastmonth:Last month"
     "\:lastquarter:Last quarter"
@@ -74,6 +76,14 @@ _hints=(
     "\:nocolor:Force color off, even if connected to a TTY"
     "\:quarter:This quarter"
     "\:quiet:Turns off all feedback. For automation"
+    "\:monday:Previous monday"
+    "\:tuesday:Previous tuesday"
+    "\:wednesday:Previous wednesday"
+    "\:thursday:Previous thursday"
+    "\:friday:Previous friday"
+    "\:saturday:Previous saturday"
+    "\:sunday:Previous sunday"
+    "\:today:The 24 hours of the current day"
     "\:week:This week"
     "\:year:This year"
     "\:yes:Overrides confirmation by answering \'yes\' to the questions"
@@ -81,39 +91,35 @@ _hints=(
 )
 
 _commands=(
-    'annotate:add an annotation to intervals'
-    'cancel:cancel time tracking'
-    'config:get and set Timewarrior configuration'
-    'continue:resume tracking of existing interval'
-    'day:Summarize the tracked and untracked time over the day'
-    'day:shows a chart depicting a single day (today by default)'
-    'delete:delete intervals'
-    'diagnostics:show diagnostic information'
-    'export:export tracked time in JSON'
-    'extensions:list available extensions'
-    'fill:adjust intervals to fill in surrounding gaps'
-    'gaps:display time tracking gaps'
-    'get:display DOM values'
-    'help:display help'
-    'join:join intervals'
-    'lengthen:lengthen intervals'
-    'modify:change start or end date of an interval'
-    'month:Summarize the tracked and untracked time over a month'
-    'month:shows a chart depicting a single month (current month by default)'
-    'move:change interval start-time'
-    'report:run an extension report'
-    'resize:set interval duration'
-    'retag:replace all tags in intervals'
-    'shorten:shorten intervals'
-    'show:display configuration'
-    'split:split intervals'
-    'start:start time tracking'
-    'stop:stop time tracking'
-    'summary:display a time-tracking summary'
-    'tag:add tags to intervals'
-    'tags:display a list of tags'
-    'track:add intervals to the database'
-    'undo:revert the last Timewarrior command'
-    'untag:remove tags from intervals'
-    'week:shows a chart depicting a single week (current week by default)'
+    'annotate:Add annotation to intervals'
+    'cancel:Cancel time tracking'
+    'config:Get and set Timewarrior configuration'
+    'continue:Resume tracking of existing interval'
+    'day:Display day chart'
+    'delete:Delete intervals'
+    'diagnostics:Show diagnostic information'
+    'export:Export tracked time in JSON'
+    'extensions:List available extensions'
+    'get:Display DOM values'
+    'help:Display help'
+    'join:Join intervals'
+    'lengthen:Lengthen intervals'
+    'modify:Change start or end time of an interval'
+    'month:Display month chart'
+    'move:Change interval start-time'
+    'report:Run an extension report'
+    'resize:Set interval duration'
+    'retag:Replace tags in intervals'
+    'shorten:Shorten intervals'
+    'show:Display configuration'
+    'split:Split intervals'
+    'start:Start time tracking'
+    'stop:Stop time tracking'
+    'summary:Display a time-tracking summary'
+    'tag:Add tags to intervals'
+    'tags:Display a list of tags'
+    'track:Add intervals to the database'
+    'undo:Undo Timewarrior commands'
+    'untag:Remove tags from intervals'
+    'week:Display week chart'
 )
